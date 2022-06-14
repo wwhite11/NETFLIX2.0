af7329db7e05d28f8ad27acb1b77bcf9cac7b5c9
wwhite11 <winstonrwhite@gmail.com>
2022-06-14 12:21:10 -0400

    deployed to firebase

---
+++ netflix2.0-app/.history/src/firebase_20220614120816.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCbEAoROXX70q-C5VFY1fKH-KwEDfZ3l8",
+    authDomain: "netflix-app-94630.firebaseapp.com",
+    projectId: "netflix-app-94630",
+    storageBucket: "netflix-app-94630.appspot.com",
+    messagingSenderId: "801681122662",
+    appId: "1:801681122662:web:e1a5f77cbe7c3e81fdd4a7"
+  };
+
+
+  
